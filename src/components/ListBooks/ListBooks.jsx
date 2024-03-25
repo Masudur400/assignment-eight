274a7b8f5ae73526ef000cb00e5f6b7112e4605a
Masudur400 <md8004210@gmail.com>
2024-03-26 01:39:57 +0600

    last update

--- src/components/ListBooks/ListBooks.jsx
+++ src/components/ListBooks/ListBooks.jsx
@@ -1,8 +1,24 @@
- 
+
+import { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+
 const ListBooks = () => {
+    const [tabIndex, setTabIndex] = useState(0);
     return (
         <div>
-            <h3 className="text-4xl">List books</h3>
+            <div className='flex justify-center my-10 items-center bg-gray-100 rounded-xl md:py-20 py-10 '>
+            <h3 className="text-4xl font-bold">  Books</h3>
+            </div>
+            <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Read Books</Tab>
+                    <Tab>Wishlist Books</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
