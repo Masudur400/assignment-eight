63f3be972b74f1630abd66c9cc790b968652c785
Masudur400 <md8004210@gmail.com>
2024-03-26 05:49:30 +0600

    last update

--- src/components/ReadBooks/ReadBooks.jsx
+++ src/components/ReadBooks/ReadBooks.jsx
@@ -1,24 +1,50 @@
- 
 
+import { IoLocationOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { RiPagesLine } from "react-icons/ri";
 import PropTypes from 'prop-types'
+import { Link } from "react-router-dom";
 
 
-const ReadBooks = ({book}) => { 
-    const {bookName,author,image,review,totalPages,rating,category,tags,publisher,year_of_publishing}=book;
+const ReadBooks = ({ book }) => {
+    const {bookId, bookName, author, image, review, totalPages, rating, category, tags, publisher, year_of_publishing } = book;
     return (
-        <div className="mb-5 md:flex border p-6 rounded-xl">
-              <div className='bg-gray-100 p-6 rounded-lg'>
+        <div className="mb-5 md:flex border p-6 rounded-xl gap-10">
+            <div className='bg-gray-100 p-6 rounded-lg'>
                 <img className='md:w-60' src={image} alt="" />
-              </div>
+            </div>
 
-              <div>
+            <div>
+                <p className='text-3xl font-bold mt-5'>{bookName}</p>
+                <p className='text-xl mt-5'>By : {author}</p>
+                <div className='flex gap-4 mt-5'>
+                    <div className='flex gap-3'>
+                        <p className='font-bold'>Tags : </p>
+                        {
+                            tags.map((tag, idx) => <p className='  px-2 py-1 rounded-full text-green-500 bg-green-50' key={idx}>{tag}</p>)
+                        }
+                    </div>
+                    <div>
+                        <p className="flex gap-2 items-center justify-center"><IoLocationOutline></IoLocationOutline>year of publishing : {year_of_publishing}</p>
+                    </div>
 
-              </div>
+                </div>
+                <div className='flex gap-7 mt-5'>
+                    <p className='flex justify-center items-center'><CgProfile></CgProfile>Publisher : {publisher}</p>
+                    <p className='flex justify-center items-center'><RiPagesLine></RiPagesLine>TotalPages : {totalPages}</p>
+                </div>
+                <div className="divider"></div>
+                <div className='flex gap-3'>
+                    <p className="bg-blue-50 text-blue-500 px-3 py-2 rounded-full">category : {category}</p>
+                    <p className="bg-orange-50 text-orange-500 px-3 py-2 rounded-full">rating : {rating}</p> 
+                    <Link to={`/book/${bookId}`} className='bg-green-500 px-3 py-2 rounded-full text-white font-bold text-center'> <button>View Details</button></Link>
+                </div>
+            </div>
         </div>
     );
 };
 
-ReadBooks.propTypes={
+ReadBooks.propTypes = {
     book: PropTypes.object
 }
 
